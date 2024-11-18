0adfaff7edc45681f5fefb2746d2bea24eac914b
rohit chavhan <rohitchavhan46@gmail.com>
2024-11-18 17:25:08 +0530

    comp 1.11*: unicafe step 6

--- part-1/ex-1.1/src/App.jsx
+++ part-1/ex-1.1/src/App.jsx
@@ -1,34 +1,45 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
 
 const Button = (prop) => <button onClick={prop.atClick}>{prop.value}</button>;
 
-// eslint-disable-next-line react/prop-types
-const Stats = ({ text, total, calc, sym }) => {
-  if (total > 0) {
-    return (
-      <p>
-        {text}: {total > 0 ? calc().toFixed(12) : 0} {sym}
-      </p>
-    );
-  }
-};
-
-// eslint-disable-next-line react/prop-types
-const StatisticLine = ({ Titles, total, reviews }) => {
+const StatisticLine = ({
+  Titles,
+  total,
+  reviews,
+  doMathFunc,
+  logicMathNames,
+}) => {
+  console;
   if (total > 0) {
     return (
       <>
-        <p>
-          {Titles[0]} {reviews[0]}
-        </p>
-        <p>
-          {Titles[1]}
-          {reviews[1]}
-        </p>
-        <p>
-          {Titles[2]}
-          {reviews[2]}
-        </p>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <td>{Titles[0]}</td>
+                <td>{reviews[0]}</td>
+              </tr>
+              <tr>
+                <td>{Titles[1]}</td>
+                <td>{reviews[1]}</td>
+              </tr>
+              <tr>
+                <td>{Titles[2]}</td>
+                <td>{reviews[2]}</td>
+              </tr>
+              <tr>
+                <td>{logicMathNames[0]}:</td>
+                <td>{doMathFunc[0]().toFixed(1)}</td>
+              </tr>
+              <tr>
+                <td>{logicMathNames[1]}:</td>
+                <td>{doMathFunc[1]().toFixed(1)} </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </>
     );
   }
@@ -41,55 +52,52 @@ const App = () => {
   const [bad, setBad] = useState(0);
   const [total, setTotal] = useState(0);
 
-  let feedsRay = ['good', 'neutral', 'bad'];
-  let statRay = ['average', 'positive'];
+  const calcAverage = () => {
+    let neutx = good - bad;
+    return neutx / total;
+  };
 
   const calcPositive = () => {
     let x = good / total;
     return x * 100;
   };
 
-  const calcAverage = () => {
-    let neutx = good - bad;
-    return neutx / total;
-  };
+  let feedsRay = ['good', 'neutral', 'bad'];
+  let statRay = ['average', 'positive'];
+  let calcFunctions = [calcAverage, calcPositive];
 
   return (
     <div>
-      <div>
-        <h1>feedback</h1>
-        <Button
-          value={feedsRay[0]}
-          atClick={() => {
-            setGood(good + 1);
-            setTotal(total + 1);
-          }}
-        />
-        <Button
-          value={feedsRay[1]}
-          atClick={() => {
-            setNeutral(neutral + 1);
-            setTotal(total + 1);
-          }}
-        />
-        <Button
-          value={feedsRay[2]}
-          atClick={() => {
-            setBad(bad + 1);
-            setTotal(total + 1);
-          }}
-        />
-        <h2>statistics</h2>
-      </div>
-
+      <h1>feedback</h1>
+      <Button
+        value={feedsRay[0]}
+        atClick={() => {
+          setGood(good + 1);
+          setTotal(total + 1);
+        }}
+      />
+      <Button
+        value={feedsRay[1]}
+        atClick={() => {
+          setNeutral(neutral + 1);
+          setTotal(total + 1);
+        }}
+      />
+      <Button
+        value={feedsRay[2]}
+        atClick={() => {
+          setBad(bad + 1);
+          setTotal(total + 1);
+        }}
+      />
+      <h2>statistics</h2>
       <StatisticLine
         Titles={feedsRay}
         total={total}
         reviews={[good, neutral, bad]}
+        doMathFunc={calcFunctions}
+        logicMathNames={statRay}
       />
-
-      <Stats text={statRay[0]} total={total} calc={calcAverage} sym={''} />
-      <Stats text={statRay[1]} total={total} calc={calcPositive} sym={'%'} />
     </div>
   );
 };
