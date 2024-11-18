89da1f204458d98345bddaeea19e5a1cf5166baf
rohit chavhan <rohitchavhan46@gmail.com>
2024-11-18 15:22:40 +0530

    comp 1.7: unicafe step 2

--- part-1/ex-1.1/src/App.jsx
+++ part-1/ex-1.1/src/App.jsx
@@ -6,8 +6,19 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
   let feedsRay = ['good', 'neutral', 'bad'];
+
+  const calcPositive = () => {
+    let x = good / total;
+    return x;
+  };
+
+  const calcAverage = () => {
+    let neutx = good - bad;
+    return neutx / total;
+  };
 
   return (
     <div>
@@ -16,24 +27,30 @@ const App = () => {
         value={feedsRay[0]}
         atClick={() => {
           setGood(good + 1);
+          setTotal(total + 1);
         }}
       />
       <Button
         value={feedsRay[1]}
         atClick={() => {
           setNeutral(neutral + 1);
+          setTotal(total + 1);
         }}
       />
       <Button
         value={feedsRay[2]}
         atClick={() => {
           setBad(bad + 1);
+          setTotal(total + 1);
         }}
       />
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total} </p>
+      <p>average: {total > 0 ? calcAverage().toFixed(12) : 0} </p>
+      <p>positive: {total > 0 ? `${calcPositive().toFixed(12)} %` : 0} </p>
     </div>
   );
 };
