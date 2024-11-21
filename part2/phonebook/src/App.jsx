12f0d3208d942f8c38463b9aced9c0d17c11193d
rohit-chavhan <rohitchavhan46@gmail.com>
2024-11-21 19:21:11 +0530

    complete exercise 2.6 - 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Lists from './components/Lists';
 import Form from './components/Forms';
 import findEqualEl from './useSearchFilter';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newPerson, setNewPerson] = useState({ name: '', number: '' });
 
   const [search, setSearch] = useState('');
   const [find, setFind] = useState([]);
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  };
+  useEffect(hook, []);
 
   const updateName = (event) =>
     setNewPerson({ name: event.target.value, number: newPerson.number });
